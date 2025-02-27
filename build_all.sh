690de50749a0e7c2be93e7f0820afb793ad5981f
0x-Matthias <138510020+0x-Matthias@users.noreply.github.com>
2025-02-27 17:56:16 +0100

    fixing line endings

--- build_all.sh
+++ build_all.sh
@@ -4,3 +4,4 @@
 (cd /root/challenges/cyberdayCTF/web/42SQL/build/ && docker-compose build)
 (cd /root/challenges/cyberdayCTF/web/Jarred/build/ && docker-compose build)
 (cd /root/challenges/cyberdayCTF/web/Peek/build/ && docker-compose build)
+
