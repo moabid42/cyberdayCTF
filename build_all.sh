c03b5db3a1f2014451068e81a46addf0290dfa0c
0x-Matthias <138510020+0x-Matthias@users.noreply.github.com>
2025-02-27 17:46:50 +0100

    scripts

--- build_all.sh
+++ build_all.sh
@@ -1,12 +1,12 @@
-cd ~/challenges/cyberdayCTF/misc/PerfectMovement/
+cd /root/challenges/cyberdayCTF/misc/PerfectMovement/
 docker-compose build
-cd ~/challenges/cyberdayCTF/misc/Pythonic/build/
+cd /root/challenges/cyberdayCTF/misc/Pythonic/build/
 docker-compose build
-cd ~/challenges/cyberdayCTF/misc/QRCodes/
+cd /root/challenges/cyberdayCTF/misc/QRCodes/
 docker-compose build
-cd ~/challenges/cyberdayCTF/web/42SQL/build/
+cd /root/challenges/cyberdayCTF/web/42SQL/build/
 docker-compose build
-cd ~/challenges/cyberdayCTF/web/Jarred/build/
+cd /root/challenges/cyberdayCTF/web/Jarred/build/
 docker-compose build
 cd /root/challenges/cyberdayCTF/web/Peek/build/
 docker-compose build
