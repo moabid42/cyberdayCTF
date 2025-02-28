2c65546e950e66f65ba98fb54b41032e46c85e6a
moabid42 <moabid@student.42heilbronn.de>
2025-02-28 09:31:38 +0100

    added deployement of pwny_vars and modified the image name of shell

--- pwn/pwny_vars/build/pwny.c
+++ pwn/pwny_vars/build/pwny.c
@@ -8,16 +8,17 @@ struct frame {
 	unsigned long x;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	struct frame f;
 	memset(&f, 0, sizeof(f));
 
+    printf("Welcome to Pwny vars\n");
 	printf("> ");
 	fflush(stdout);
 
 	read(STDIN_FILENO, &f.buffer[0], 256);
 
-	printf("x = %lx\n", f.x);
 	if (f.x == (unsigned long)0xdeadbabebeefc0deUL) {
 		system("cat flag.txt");
 	}
